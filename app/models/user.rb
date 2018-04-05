e47496478e408cd8104d7508df553d1a9b380072
Cora Quezada <corafenice1013@gmail.com>
2018-04-05 17:42:13 -0400

    added models for message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 class User < ApplicationRecord
   has_many :enrollments
 
-  # before_save { self.email = email.downcase }
-  #
-  # valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  #
-  # validates(:name, presence: true, length: { minimum: 2, maximum: 25  })
-  #
-  # validates(:email, presence: true, format: { with: valid_email }, length: {minimum: 5, maximum: 25}, uniqueness: { case_sensitive: false})
-  #
-  # validates :password, presence: true, length: { minimum: 6 }
-  #
-  # has_secure_password
+  before_save { self.email = email.downcase }
+
+  valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates(:name, presence: true, length: { minimum: 2, maximum: 25  })
+
+  validates(:email, presence: true, format: { with: valid_email }, length: {minimum: 5, maximum: 25}, uniqueness: { case_sensitive: false})
+
+  validates :password, presence: true, length: { minimum: 6 }
+
+   has_secure_password
+
+# def name
+#   # email.split('@')[0]
+# end
+
+
+
 end
